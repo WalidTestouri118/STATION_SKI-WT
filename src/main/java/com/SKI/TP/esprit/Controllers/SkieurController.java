1d2cc8cc5b96dcab35e5577ad79cd842430e22b3
WalidTestouri118 <74499389+WalidTestouri118@users.noreply.github.com>
2023-04-11 11:17:32 +0200

    Update SkieurController.java
    nested object

--- src/main/java/com/SKI/TP/esprit/Controllers/SkieurController.java
+++ src/main/java/com/SKI/TP/esprit/Controllers/SkieurController.java
@@ -1,12 +1,20 @@
 package com.SKI.TP.esprit.Controllers;
 
+import com.SKI.TP.esprit.Entities.Cours;
+import com.SKI.TP.esprit.Entities.Inscription;
 import com.SKI.TP.esprit.Entities.Skieur;
+import com.SKI.TP.esprit.Repositories.InscriptionRepository;
+import com.SKI.TP.esprit.Repositories.SkieurRepository;
 import com.SKI.TP.esprit.services.ISkieurService;
+import org.aspectj.bridge.IMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+
+import static org.springframework.util.Assert.notNull;
 
 @RestController
 @RequestMapping("skieur")
@@ -68,4 +76,22 @@ public class SkieurController {
         return iSkieurService.assignSkierToInscription(numSkieur, numInscription);
     }
 
+
+    @PostMapping("")
+    public Skieur addSkierAndAssignToCourse(Skieur skieur){
+        assert.notnull(skieur.getabonnement(),"abonnement must be entered");
+        assert.notNull(skieur.getinscription(),"inscription must be entered");
+        Set<Inscription> inscriptions=skieur.getInscriptions();
+        inscriptions.forEach(inscription -> {
+            assert.notNull(inscription.getCours().getNumCours(), "cour must be entered");
+            Cours cours=coursRepository.findbyid(inscription.getCours().getNumCours().orElse(null))
+            assert.notNull(cours,"no coutr found with this is !!!!!");
+            inscriptions.setCours(cours);
+            SkieurRepository.saveAndFlush(skieur);
+            inscription.setSkieur(skieur);
+            InscriptionRepository.Save(inscription);
+        });
+
+        return skieur;
+    }
 }
